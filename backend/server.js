1b8028fb3f23feb3efcc75a805ad405b5c5442f9
Sonam Priya <sonamjha0409@gmail.com>
2025-05-23 21:04:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ connectCloudinary()
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+  {
+    origin : "https://hospital-management-system-beryl-one.vercel.app/"
+  }
+))
 
 // api endpoints
 app.use("/api/user", userRouter)
